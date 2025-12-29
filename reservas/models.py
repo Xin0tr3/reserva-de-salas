9124c48381c5fe7614aad0cfee2d8600bbf3c981
Matias Enrique Cabrera Quevedo <mcabreraq@utem.cl>
2025-12-29 15:15:41 -0300

    Nuevo modelo y cambios
    Solicitaron un sistema de invitados donde se pudiera obtener un Excel que tuviera todos los invitados y la informacion respectiva de cada uno, esto es solo una modificacion al apartado de modelos que agrega el modelo Invitados, en otra rama esta el apartado de Excel

--- reservas/models.py
+++ reservas/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Salas(models.Model):
+    pisos_opciones = [
+        ('PISO 1', 'Piso 1'),
+        ('PISO 2', 'Piso 2'),
+    ]
+
     nombre_sala = models.CharField(max_length=100)
     numero_sala = models.PositiveIntegerField(default=1)
     capacidad_maxima = models.PositiveIntegerField(default=1)
@@ -9,6 +14,7 @@ class Salas(models.Model):
     descripcion_sala = models.TextField()
     hora_minima = models.TimeField()
     hora_maxima= models.TimeField()
+    piso_sala = models.CharField(max_length=15, choices=pisos_opciones, default='PISO 1')
 
 class Reservas(models.Model):
     estado_opciones = [
@@ -22,11 +28,30 @@ class Reservas(models.Model):
     nombre_reservante = models.CharField(max_length=255)
     departamento = models.CharField(max_length=100)
     email = models.EmailField()
-    nombre_evento = models.CharField( max_length=100)
+    nombre_evento = models.CharField(max_length=100)
     descripcion_evento = models.TextField()
-    equipamiento_extra = models.TextField()
+    equipamiento_extra = models.TextField(blank=True)
     estado_reserva = models.CharField(max_length=15, choices=estado_opciones, default='PENDIENTE')
     codigo = models.CharField(max_length=50, unique=True, null=True, blank=False)
     rut = models.CharField(max_length=12,blank=False)
+    
     sala_reservada = models.ForeignKey(Salas, on_delete=models.CASCADE)
 
+class Invitados(models.Model):
+    rol_opciones = [
+        ('FUNCIONARIO', 'Funcionario'),
+        ('ACADEMICO', 'Academico'),
+        ('ALUMNO', 'Alumno'),
+    ]
+    sexo_opciones = [
+        ('HOMBRE', 'Hombre'),
+        ('MUJER', 'Mujer'),
+        ('SIN ESPECIFICAR', 'Sin especificar')
+    ]
+    nombre_invitado = models.CharField(max_length=100)
+    apellidos_invitado = models.CharField(max_length=100)
+    rol = models.CharField(max_length=15, choices=rol_opciones, default='FUNCIONARIO')
+    carrera = models.CharField(max_length=100, blank=True)
+    facultad = models.CharField(max_length=100, blank=True)
+    sexo = models.CharField(max_length=15, choices=sexo_opciones, default='SIN ESPECIFICAR')
+    codigo_reserva = models.ForeignKey(Reservas, on_delete=models.CASCADE)
