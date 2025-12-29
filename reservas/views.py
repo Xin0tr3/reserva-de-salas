6909d0f48e5346f65ca627019e340ceb307a238d
Matias Enrique Cabrera Quevedo <mcabreraq@utem.cl>
2025-12-29 16:38:51 -0300

    Base del excel
    Aun no hago pruebas, estaba viendo como funcionaba la librería y asegurándome de que no faltaran mas cosas en los modelos, de todas formas falta testeo y asegurarse con terceros

--- reservas/views.py
+++ reservas/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from .models import Reservas
+from openpyxl import Workbook
+from .models import Reservas, Invitados
 
 def gestionar_reserva(request, codigo, accion):
 
@@ -21,3 +22,42 @@ def gestionar_reserva(request, codigo, accion):
 
     context = {'reserva': reserva, 'mensaje': mensaje}
     return render(request, 'reservas/gestion_exitosa.html', context)
+
+def invitados_excel(request, reserva_id):
+    reserva = Reservas.objects.get(id=reserva_id)
+    invitados_r = Invitados.objects.filter(codigo_reserva=reserva)
+
+    wb = Workbook()
+    ws = wb.active
+
+    Header = ['Nombre Invitado', 'Apellidos Invitados', 'Rol', 'Carrera', 'Facultad', 'Sexo', 'Codigo Reserva']
+    ws.append(['Invitados del evento: ', reserva.nombre_evento])
+    ws.append(['Sala: ', reserva.sala_reservada.nombre_sala])
+    ws.append([])
+    ws.append(Header)
+
+    for invitados in invitados_r:
+        ws.append[
+            invitados.nombre_invitado,
+            invitados.apellidos_invitado,
+            invitados.get_rol_display(),
+            invitados.carrera,
+            invitados.facultad,
+            invitados.get_sexo_display(),
+            invitados.codigo_reserva
+        ]
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    )
+    response['Content-Disposition'] = f'attachment; filename="invitados_{reserva.codigo}.xlsx"'
+
+    wb.save(response)
+    return response
+
+def agregar_invitados(request, reserva_id):
+    reserva = Reservas.objects.get(id=reserva_id)
+    capacidad_max = reserva.sala_reservada.capacidad_maxima
+    contador = Invitados.objects.filter(codigo_reserva=reserva).count()
+
+    if contador >= capacidad_max:
+        return HttpResponse ("Error: Se a alcanzado la capacidad maxima de la sala")
